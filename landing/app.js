170350bf2a74ebd2fd09b951a561924f47c86cd7
Anthony <rbcxdd@gmail.com>
2019-04-11 21:45:56 -0500

    minor positioning fixes and menu scrolling onclick

--- landing/app.js
+++ landing/app.js
@@ -4,10 +4,10 @@ function menu() {
   document.getElementsByClassName('bg2')[0].classList.toggle("mobile_menu_active")
   var i = 0;
   var load = setInterval(function () {
-    document.getElementsByClassName('menu_skill')[i].classList.toggle("menu_item_active")
+    document.getElementsByClassName('menu_item')[i].classList.toggle("menu_item_active")
     i++
-    if (i == 4) {
-      clearInterval( load );
+    if (i == 3) {
+      clearInterval(load);
     }
   }, 70)
 
