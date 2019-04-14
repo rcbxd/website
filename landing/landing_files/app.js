098fde2bf521f2c0c492fb3b68b96472f0f266db
Anthony <rbcxdd@gmail.com>
2019-04-13 20:41:45 -0500

    syntax fixes

--- landing/landing_files/app.js
+++ landing/landing_files/app.js
@@ -1,11 +1,11 @@
 function menu() {
-  document.getElementsByClassName('bg_sec')[0].classList.toggle("mobile_menu_active")
-  document.getElementsByClassName('bg')[0].classList.toggle("mobile_menu_active")
-  document.getElementsByClassName('bg2')[0].classList.toggle("mobile_menu_active")
+  document.getElementsByClassName('bg_sec')[0].classList.toggle("mobile_menu_active");
+  document.getElementsByClassName('bg')[0].classList.toggle("mobile_menu_active");
+  document.getElementsByClassName('bg2')[0].classList.toggle("mobile_menu_active");
   var i = 0;
   var load = setInterval(function () {
-    document.getElementsByClassName('menu_item')[i].classList.toggle("menu_item_active")
-    i++
+    document.getElementsByClassName('menu_item')[i].classList.toggle("menu_item_active");
+    i++;
     if (i == 3) {
       clearInterval(load);
     }
