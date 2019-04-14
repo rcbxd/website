066660a1a37f9d6a28da9abddbe33cb695d9479c
Anthony <rbcxdd@gmail.com>
2019-04-13 22:50:50 -0500

    added cursor

--- landing/landing_files/app.js
+++ landing/landing_files/app.js
@@ -1,7 +1,11 @@
+const cursor = document.getElementById("cursor")
+var h = cursor.offsetHeight - 2
+
 function menu() {
   document.getElementsByClassName('bg_sec')[0].classList.toggle("mobile_menu_active");
   document.getElementsByClassName('bg')[0].classList.toggle("mobile_menu_active");
   document.getElementsByClassName('bg2')[0].classList.toggle("mobile_menu_active");
+  cursor.classList.toggle("cursor_invert")
   var i = 0;
   var load = setInterval(function () {
     document.getElementsByClassName('menu_item')[i].classList.toggle("menu_item_active");
@@ -12,3 +16,30 @@ function menu() {
   }, 70)
 
 }
+
+const cursorHighlight = (s) => {
+  if (s) {
+    cursor.classList.add("expand")
+  }
+  cursor.classList.add("cursor_highlight")
+}
+
+const cursorBack = () => {
+  cursor.classList.remove("expand")
+  cursor.classList.remove("cursor_highlight")
+}
+
+document.addEventListener("mousemove", e => {
+  cursor.setAttribute("style", "top: " + (e.pageY - (h / 2)) + "px; left: " + (e.pageX - (h / 2)) + "px");
+})
+
+document.addEventListener("click", () => {
+  cursor.classList.add("expand")
+  window.setTimeout(() => {
+    cursor.classList.remove("expand")
+  }, 500)
+})
+
+window.addEventListener("load", () => {
+  cursor.setAttribute("style", "top: " + ((window.innerHeight / 2) - (h / 2)) + "px; left: " + ((window.innerWidth / 2) - (h / 2) + "px"))
+})
