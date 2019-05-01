6e76a50a6b0b7ba5c3a77f1fbe8fcbe02c2aab1c
Anthony <rbcxdd@gmail.com>
2019-04-30 19:02:13 -0500

    adding variables to scss and changing folder name

--- static/app.js
+++ static/app.js
@@ -1,3 +1,6 @@
+// js app core
+
+
 const cursor = document.getElementById("cursor");
 const h = cursor.offsetHeight - 2;
 var sc = true;
@@ -14,7 +17,7 @@ const menu = () => {
     .classList.toggle("mobile_menu_active");
   cursor.classList.toggle("cursor_invert");
   var i = 0;
-  var load = setInterval(function () {
+  var load = setInterval(function () { // menu items loader
     document
       .getElementsByClassName("menu_item")[i].classList.toggle("menu_item_active");
     i++;
@@ -24,33 +27,33 @@ const menu = () => {
   }, 100);
 };
 
-const cursorHighlight = s => {
+const cursorHighlight = s => { // highlight the cursor if not a link, expand if a link
   if (s) {
     cursor.classList.add("expand");
   }
   cursor.classList.add("cursor_highlight");
 };
 
-const cursorBack = () => {
+const cursorBack = () => { // get the cursor to its inial state
   cursor.classList.remove("expand");
   cursor.classList.remove("cursor_highlight");
 };
 
-document.addEventListener("mousemove", e => {
+document.addEventListener("mousemove", e => { // cursor movement
   cursor.setAttribute(
     "style",
     "top: " + (e.pageY - h / 2) + "px; left: " + (e.pageX - h / 2) + "px"
   );
 });
 
-document.addEventListener("click", () => {
+document.addEventListener("click", () => { // cursor expand on click
   cursor.classList.add("expand");
   window.setTimeout(() => {
     cursor.classList.remove("expand");
   }, 500);
 });
 
-window.addEventListener("load", () => {
+window.addEventListener("load", () => { // put the cursor in the middle of the screen onload
   cursor.setAttribute(
     "style",
     "top: " +
@@ -60,23 +63,13 @@ window.addEventListener("load", () => {
   );
 });
 
-
-
-window.addEventListener("wheel", (e) => {
-  if (e.deltaY < 0 && sc && window.scrollY == 0) {
-    sc = false;
-    menu()
-  }
-})
-
-
-window.addEventListener("scroll", (e) => {
+window.addEventListener("scroll", (e) => { // mobile menu on scroll to the top
   if (sc && window.scrollY == 0 && isMobile()) {
     menu()
     sc = false;
   }
 })
 
-const isMobile = () => {
+const isMobile = () => { // return if the device is mobile
   return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
 }
