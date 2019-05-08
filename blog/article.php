29bb35dc5d7b7ca5c84935f8bbc631a3869f65c5
Anthony Skorupskyy <rbcxdd@gmail.com>
2019-05-07 19:18:50 -0500

    query added

--- blog/article.php
+++ blog/article.php
@@ -31,7 +31,7 @@ echo "<body>";
 
     $views = $article['views']+1;
     
-    $sql = "UPDATE blog set views = " . $views . " WHERE article_id = " . $id;
+    $sql = mysqli_query("UPDATE blog set views = " . $views . " WHERE article_id = " . $id);
 
     echo '<h2 class="data">' . $views . ' views. </h2>';
 
