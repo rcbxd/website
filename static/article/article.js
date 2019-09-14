843e72b6fdfdced2b4e5185550627412e95615ab
rcbxd <rbcxdd@gmail.com>
2019-09-14 16:16:19 -0500

    comments working

--- static/article/article.js
+++ static/article/article.js
@@ -116,3 +116,18 @@ function gotoDefiniton(event) {
     var url = `https://google.com/search?q=${event.target.getAttribute('search-query')}`
     window.location.href = url;
 }
+
+window.addEventListener('load', function () {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener('submit', function (event) {
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    });
+}, false);
