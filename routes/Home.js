0221fab3274326936088a04d41034d99d32e4d83
rcbxd <rbcxdd@gmail.com>
2020-03-08 17:59:54 -0500

    renewed design

--- routes/Home.js
+++ routes/Home.js
@@ -4,15 +4,19 @@ const Post = require("../models/Post");
 const handleServerError = require("../util/serverError");
 
 router.get("/", (req, res, next) => {
-  Post.findAll()
-    .then(posts => {
-      res.render("views/index", {
-        posts: posts
-      });
-    })
-    .catch(err => {
-      handleServerError(res, true);
-    });
+  res.render("views/index");
+});
+
+router.get("/projects/", (req, res, next) => {
+  res.render("views/projects");
+});
+
+router.get("/personal/", (req, res, next) => {
+  res.render("views/personal");
+});
+
+router.get("/skills/", (req, res, next) => {
+  res.render("views/skills");
 });
 
 module.exports = router;
